2a8ee6b05845784518c814d8629a80edcf96b8b1
BumpeiShimada <6376404+BumpeiShimada@users.noreply.github.com>
2026-05-26 20:22:48 +0900

    Fix mdpoke checksum

--- Formula/mdpoke.rb
+++ Formula/mdpoke.rb
@@ -2,7 +2,7 @@ class Mdpoke < Formula
   desc "Terminal Markdown viewer for poking around long Markdown documents"
   homepage "https://github.com/BumpeiShimada/mdpoke"
   url "https://github.com/BumpeiShimada/mdpoke/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "ここにsha256"
+  sha256 "78bc673fe9622b26f702fc7c1a00ab00111ff9047701283f59b10f6e76701d58"
   license "MIT"
 
   depends_on "go" => :build
