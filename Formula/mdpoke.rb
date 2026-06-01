2f10dcdab630447fd71e68bf0d87ac670aa0c1c1
Bumpei Shimada <6376404+BumpeiShimada@users.noreply.github.com>
2026-06-01 21:00:59 +0900

    update

--- Formula/mdpoke.rb
+++ Formula/mdpoke.rb
@@ -2,7 +2,7 @@ class Mdpoke < Formula
   desc "Terminal Markdown viewer for poking around long Markdown documents"
   homepage "https://github.com/BumpeiShimada/mdpoke"
   url "https://github.com/BumpeiShimada/mdpoke/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "ここに新しいsha256を入れる"
+  sha256 "bd372580f8ca708cd63643ae0051a263deb230a9ca65d501e626b79aac82bad5"
 
   license "MIT"
 
